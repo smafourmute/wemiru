f9cee34fc2d277059bd0dd554223f336e472cab7
loliconazter <loliconazterproject@gmail.com>
2021-08-05 22:26:01 +0700

    Add files via upload

--- m/scripts/playlist.js
+++ m/scripts/playlist.js
@@ -5,12 +5,19 @@ trackList = [
 	album:"HATSUNE MIKU EXPO 2016 E.P.",
     src:"music/ten thousand stars.mp3"
   },
-          
+  {
+    name:"가사",
+    artist:"방탄소년단 ",
+	album:"Map of the Soul: 7",
+    src:"music/ON.mp3"
+  },     
   {
     name: "Bitter Majesty feat. 初音ミク",
     artist:"八王子P",
 	album:"Last Dance Refrain",
     src: "music/Bitter Majesty.mp3"
   }
+  
+  
 
 ];
