0d16babc5281a4abf074fc6361898f6fd4f0442e
loliconazter <loliconazterproject@gmail.com>
2021-08-05 18:55:19 +0700

    Add files via upload

---
+++ m/scripts/playlist.js
@@ -0,0 +1,16 @@
+trackList = [
+  {
+    name:"Ten Thousand Stars feat. 初音ミク",
+    artist:"CircusP",
+	album:"HATSUNE MIKU EXPO 2016 E.P.",
+    src:"music/ten thousand stars.mp3"
+  },
+          
+  {
+    name: "Bitter Majesty feat. 初音ミク",
+    artist:"八王子P",
+	album:"Last Dance Refrain",
+    src: "music/Bitter Majesty.mp3"
+  }
+
+];
